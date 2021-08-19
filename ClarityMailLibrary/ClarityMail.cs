b19d9f07cb4b578f0912d774eb1c8a32ea6748f0
Jeremy LaRose <jeremy@jeremylarose.com>
2021-08-19 07:48:27 -0400

    Added Serilog writing to file functionality

--- ClarityMailLibrary/ClarityMail.cs
+++ ClarityMailLibrary/ClarityMail.cs
@@ -1,30 +1,37 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Net;
+using System.IO;
 using System.Net.Mail;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using FluentEmail.Core;
-using FluentEmail.Core.Models;
 using FluentEmail.Razor;
 using FluentEmail.Smtp;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace ClarityEmailerLibrary
 {
-    public partial class ClarityMail : Component
+    public class ClarityMail : IClarityMail
     {
-        // Private fields necessary
+        #region Private
+        private int _id;
         private string _mailFrom;
         private string _mailTo;
         private string _mailSubject;
         private string _mailBody;
         private string _SMTPServer;
         private int _SMTPPort;
-
+    #endregion
         // Public properties
+        #region Public
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
         public string MailFrom
         {
             get => _mailFrom;
@@ -62,7 +69,7 @@ namespace ClarityEmailerLibrary
         }
 
         private static Dictionary<string, (int, bool)> _recipientsToReattempt = new();
-
+        
         public ClarityMail()
         {
             _mailTo = "";
@@ -72,115 +79,64 @@ namespace ClarityEmailerLibrary
             _SMTPPort = 25;
             _SMTPServer = "";
         }
-        
-        public async Task RetrySendMessage(string recipientMailbox, string senderMailbox, string body, string subject, int retries)
+
+        #endregion
+
+        public async Task SendAsync(string recipientName, string recipientEmailAddress, string senderMailbox, string subject, string body, int retries)
         {
-            var sender = new SmtpSender(() => new SmtpClient("localhost")
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+            
+            var claritySettings = configuration.GetSection("ClarityMail");
+
+            var sender = new SmtpSender(() => new SmtpClient( claritySettings["Host"])
             {
+                Port = int.Parse(claritySettings["Port"]),
                 EnableSsl = false,
                 DeliveryMethod = SmtpDeliveryMethod.Network,
-                Port = 25
             });
-
+            
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .WriteTo.RollingFile(@"C:\Logs\ClarityMailLibrary-{Date}A.txt", retainedFileCountLimit: 10 ) 
+                .CreateLogger();
+            
             Email.DefaultSender = sender;
             Email.DefaultRenderer = new RazorRenderer();
 
             StringBuilder template = new();
-
-            template.AppendLine("Dear @Model.FirstName,");
+            template.AppendLine("Dear @Model.FirstName");
             template.Append(body);
-            template.AppendLine("- The Clarity Ventures Team");
+            template.Append("- The Clarity Ventures Team");
 
-            var recipientsToAttempt = _recipientsToReattempt[recipientMailbox];
-
-            while (recipientsToAttempt.Item2 == false)
-            {
-                Console.WriteLine($"Resending Emails: Retry Count: {retries}");
-                try
-                {
-                    var email = await Email
-                        .From(senderMailbox, senderMailbox)
-                        .To(recipientMailbox, recipientMailbox)
-                        .Subject(subject)
-                        .UsingTemplate(template.ToString(), new { FirstName = "Jeremy" })
-                        .SendAsync();
-                    recipientsToAttempt.Item2 = true;
-                    Console.WriteLine($"Successfully resent to {recipientMailbox}.");
-                }
-                catch (SmtpException ex)
-                {
-                    --retries;
-                    var exception = ex.Message;
-                    recipientsToAttempt.Item1 = retries;
-                    Console.WriteLine($"RetrySend: Retry sending to {recipientMailbox} failed: {retries}");
-                    if (retries <= 0)
-                    {
-                        Console.WriteLine( "Failed all retries. Continuing.");
-                        break;
-                    }
-                }
-            }
-        }
-
-        public async Task SendMessages(List<string> recipientMailboxes, string senderMailbox, string body, string subject, int retries)
-        {
-            var sender = new SmtpSender(() => new SmtpClient("localhost")
-            {
-                EnableSsl = false,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                Port = 35,
-            });
-
-            Email.DefaultSender = sender;
-            Email.DefaultRenderer = new RazorRenderer();
-
-            StringBuilder template = new();
-
-            template.AppendLine("Dear @Model.FirstName,");
-            template.Append(body);
-            template.AppendLine("- The Clarity Ventures Team");
-
-            foreach (var recipientMailbox in recipientMailboxes)
+            for (var count = 1; count <= retries; count++)
             {
                 try
                 {
-                    var email = await Email
+                    await Email
                         .From(senderMailbox, senderMailbox)
-                        .To(recipientMailbox, recipientMailbox)
+                        .To(recipientEmailAddress, recipientName)
                         .Subject(subject)
                         .UsingTemplate(template.ToString(), new { FirstName = "Jeremy" })
                         .SendAsync();
-                    Console.WriteLine($"Send to {recipientMailbox} successful!");
+
+                    Log.Information( $"SENDASYNC SUCCESS: TO:{recipientEmailAddress} FROM:{senderMailbox} SUBJECT:{subject} BODY:{body}");
+                    return;
                 }
-                catch (SmtpException ex)
+                catch (Exception ex)
                 {
                     var exception = ex.Message;
-                    Console.WriteLine($"Sending to {recipientMailbox} failed: {exception}");
-                    // Add them to the database of failed recipients to retry later
-                    _recipientsToReattempt.Add(recipientMailbox, (retries, false));
-                }
-            }
+                    Log.Warning( $"SENDASYNC FAIL: TO:{recipientEmailAddress} FROM:{senderMailbox} SUBJECT:{subject} BODY:{body}  : Attempt {count} of {retries}.");
 
-            if (_recipientsToReattempt.Count != 0)
-            {
-                Console.WriteLine($"SendMessage: There are {_recipientsToReattempt.Count} number of emails that failed to deliver.");
-                // Iterate through each person that is on the reattempt dictionary
-                foreach (var retryRecipient in _recipientsToReattempt)
-                {
-                    // If the last valid email status was false, then try to resend an email
-                    if (retryRecipient.Value.Item2 == false)
+                    if (count >= retries)
                     {
-                        // Resend email
-                        try
-                        {
-                            await RetrySendMessage(retryRecipient.Key, senderMailbox, body, subject, retries);
-                        }
-                        catch (SmtpException ex)
-                        {
-                            retries--;
-                            Console.WriteLine($"Resend failed for {retryRecipient}. Tries remaining: {retries}.");
-                        }
+                        continue;
                     }
+
+                    await Task.Delay(count * 1000);
                 }
             }
         }
---
+++ ClarityMailLibrary/IClarityMail.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace ClarityEmailerLibrary
+{
+    public interface IClarityMail
+    {
+        public Task SendAsync( string recipientName, string recipientMailbox, string senderMailbox, string body, string subject, int retries);
+    }
+}
