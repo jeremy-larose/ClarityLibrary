76d19ac8aea6a4c883525ca65fba0c778a0cc8e4
Jeremy LaRose <jeremy@jeremylarose.com>
2021-08-19 20:47:40 -0400

    Removed functionality for RazorRenderer, added HTML body.

--- ClarityMailLibrary/ClarityMail.cs
+++ ClarityMailLibrary/ClarityMail.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,8 +27,10 @@ namespace ClarityMailLibrary
         private string _mailTo;
         private string _mailSubject;
         private string _mailBody;
+        private string _mailDisplayName;
         private string _SMTPServer;
         private int _SMTPPort;
+        
     #endregion
         #region Public
 
@@ -43,6 +46,11 @@ namespace ClarityMailLibrary
             set => _mailFrom = value;
         }
 
+        public string MailDisplayName
+        {
+            get => _mailDisplayName;
+            set => _mailDisplayName = value;
+        }
         public string MailTo
         {
             get => _mailTo;
@@ -110,7 +118,8 @@ namespace ClarityMailLibrary
             var sender = new SmtpSender(() => new SmtpClient( claritySettings["Host"])
             {
                 Port = int.Parse(claritySettings["Port"]),
-                EnableSsl = false,
+                Credentials = new NetworkCredential( claritySettings["Mail"], claritySettings["Password"]),
+                EnableSsl = true,
                 DeliveryMethod = SmtpDeliveryMethod.Network,
             });
             
@@ -121,12 +130,7 @@ namespace ClarityMailLibrary
             
             Email.DefaultSender = sender;
             Email.DefaultRenderer = new RazorRenderer();
-
-            StringBuilder template = new();
-            template.AppendLine("Dear @Model.FirstName");
-            template.Append(body);
-            template.AppendLine("- The Clarity Ventures Team");
-
+            
             for (var count = 1; count <= retries; count++)
             {
                 try
@@ -135,7 +139,8 @@ namespace ClarityMailLibrary
                         .From(senderMailbox, senderMailbox)
                         .To(recipientEmailAddress, recipientName)
                         .Subject(subject)
-                        .UsingTemplate(template.ToString(), new { FirstName = $"{recipientName}" })
+                        //.UsingTemplate(template.ToString(), new { FirstName = $"{recipientName}" })
+                        .Body( body, true )
                         .SendAsync();
 
                     Log.Information( $"SEND SUCCESS: TO:{recipientEmailAddress} FROM:{senderMailbox} SUBJECT:{subject} BODY:{body}");
@@ -177,7 +182,8 @@ namespace ClarityMailLibrary
             var sender = new SmtpSender(() => new SmtpClient( claritySettings["Host"])
             {
                 Port = int.Parse(claritySettings["Port"]),
-                EnableSsl = false,
+                Credentials = new NetworkCredential( claritySettings["Mail"], claritySettings["Password"]),
+                EnableSsl = true,
                 DeliveryMethod = SmtpDeliveryMethod.Network,
             });
             
@@ -189,20 +195,16 @@ namespace ClarityMailLibrary
             Email.DefaultSender = sender;
             Email.DefaultRenderer = new RazorRenderer();
 
-            StringBuilder template = new();
-            template.AppendLine("Dear @Model.FirstName");
-            template.Append(body);
-            template.AppendLine("- The Clarity Ventures Team");
-
             for (var count = 1; count <= retries; count++)
             {
                 try
                 {
                     await Email
-                        .From(senderMailbox, senderMailbox)
+                        .From(senderMailbox, claritySettings["DisplayName"])
                         .To(recipientEmailAddress, recipientName)
                         .Subject(subject)
-                        .UsingTemplate(template.ToString(), new { FirstName = $"{recipientName}" })
+                        //.UsingTemplate(template.ToString(), new { FirstName = $"{recipientName}" })
+                        .Body( body, true )
                         .SendAsync();
 
                     Log.Information( $"SENDASYNC SUCCESS: TO:{recipientEmailAddress} FROM:{senderMailbox} SUBJECT:{subject} BODY:{body}");
@@ -222,5 +224,71 @@ namespace ClarityMailLibrary
                 }
             }
         }
+        
+        /// <summary>
+        /// Sends an email asynchronously, attempting to retry {retries} number of times upon failure. Uses development environment for testing with Papercut.
+        /// </summary>
+        /// <param name="recipientName">The name of the recipient, to insert into the "Hello {recipient}," field.</param>
+        /// <param name="recipientEmailAddress">The email address of the person to receive the email.</param>
+        /// <param name="senderMailbox">The email address of the sender.</param>
+        /// <param name="subject">The data to insert into the subject of the email.</param>
+        /// <param name="body">The body of the email, inserted using Razor Template.</param>
+        /// <param name="retries">The number of times to attempt to resend in the event of failure.</param>
+        public async Task SendTestAsync(string recipientName, string recipientEmailAddress, string senderMailbox, string subject, string body, int retries)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+            
+            var claritySettings = configuration.GetSection("ClarityMail");
+
+            var sender = new SmtpSender(() => new SmtpClient( "localhost" )
+            {
+                Port = 35,
+                Credentials = new NetworkCredential( claritySettings["Mail"], claritySettings["Password"]),
+                EnableSsl = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+            });
+            
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .WriteTo.RollingFile(@"C:\Logs\ClarityMail\ClarityMailLibrary-{Date}A.txt", retainedFileCountLimit: 10 ) 
+                .CreateLogger();
+            
+            Email.DefaultSender = sender;
+            Email.DefaultRenderer = new RazorRenderer();
+            
+            for (var count = 1; count <= retries; count++)
+            {
+                try
+                {
+                    await Email
+                        .From(senderMailbox, claritySettings["DisplayName"])
+                        .To(recipientEmailAddress, recipientName)
+                        .Subject(subject)
+                        //.UsingTemplate(template.ToString(), new { FirstName = $"{recipientName}" })
+                        .Body( body, true )
+                        .SendAsync();
+
+                    Log.Information( $"SENDASYNC SUCCESS: TO:{recipientEmailAddress} FROM:{senderMailbox} SUBJECT:{subject} BODY:{body}");
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    var exception = ex.Message;
+                    Log.Warning( $"SENDASYNC FAIL: TO:{recipientEmailAddress} FROM:{senderMailbox} SUBJECT:{subject} BODY:{body}  : Attempt {count} of {retries}.");
+
+                    if (count >= retries)
+                    {
+                        continue;
+                    }
+
+                    await Task.Delay(count * 1000);
+                }
+            }
+        }
+
     }
 }
--- ClarityMailLibrary/IClarityMail.cs
+++ ClarityMailLibrary/IClarityMail.cs
@@ -5,6 +5,7 @@ namespace ClarityMailLibrary
     public interface IClarityMail
     {
         public Task SendAsync( string recipientName, string recipientMailbox, string senderMailbox, string body, string subject, int retries);
+        public Task SendTestAsync( string recipientName, string recipientMailbox, string senderMailbox, string body, string subject, int retries);
         public void Send(string recipientName, string RecipientMailbox, string senderMailbox, string body, string subject, int retries);
     }
 }
